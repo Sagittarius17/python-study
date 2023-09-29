1f435812e515f771a0ed7c82dc99062167e4a738
shuvendu <108450702+Sagittarius17@users.noreply.github.com>
2023-09-29 23:31:24 +0530

    built-in function alternative

--- AltPrebuildFunctions/apf.py
+++ AltPrebuildFunctions/apf.py
@@ -1,3 +1,5 @@
+from custom_print import custom_print
+
 # 1))) Prebuild function "len()"
 lst = [1, 2, 3, 4]
 print(len(lst))
@@ -11,6 +13,8 @@ def custom_len(sequence):
 
 lst = [1, 2, 3, 4]
 print(custom_len(lst))
+custom_print(custom_len(lst))
+
 
 
 # 2))) Prebuild function "sum()"
@@ -26,6 +30,7 @@ def custom_sum(iterable):
 
 lst1 = [1, 2, 3, 4]
 print(custom_sum(lst1))
+
 
 
 # 3))) Prebuild function "max()"
@@ -44,6 +49,7 @@ lst2 = [1, 2, 3, 4]
 print(custom_max(lst2))
 
 
+
 # 4))) Prebuild function "min()"
 lst3 = [1, 2, 3, 4]
 print(min(lst3))
@@ -58,3 +64,71 @@ def custom_min(iterable):
 
 lst3 = [1, 2, 3, 4]
 print(custom_min(lst3))
+
+
+
+# 5))) Prebuild function "abs()"
+print(abs(-5))  # Output: 5
+
+# Alternative or without using "abs()" function:
+def custom_abs(x):
+    return x if x >= 0 else -x
+
+print(custom_abs(-5))  # Output: 5
+
+
+
+# 6))) Prebuild function "str()"
+num = 123
+print(str(num))  # Output: '123'
+
+# Alternative or without using "str()" function:
+def custom_str(obj):
+    return obj.__str__()
+
+num = 123
+print(custom_str(num))  # Output: '123'
+
+
+
+# 7))) Prebuild function "round()"
+print(round(5.678, 2))  # Output: 5.68
+
+# Alternative or without using "round()" function:
+def custom_round(number, ndigits=None):
+    multiplier = 10 ** ndigits if ndigits is not None else 1
+    return int(number * multiplier + 0.5 if number > 0 else number * multiplier - 0.5) / multiplier
+
+print(custom_round(5.678, 2))  # Output: 5.68
+
+
+
+# 8))) Prebuild function "type()"
+print(type(5))  # Output: <class 'int'>
+
+# Alternative or without using "type()" function:
+def custom_type(obj):
+    return obj.__class__
+
+print(custom_type(5))  # Output: <class 'int'>
+
+
+
+# 9))) Prebuild function "pow()"
+print(pow(2, 3))  # Output: 8
+
+# Alternative or without using "pow()" function:
+def custom_pow(base, exponent):
+    return base ** exponent
+
+print(custom_pow(2, 3))  # Output: 8
+
+
+
+# 5))) Prebuild function "abs()"
+
+
+# 5))) Prebuild function "abs()"
+
+
+
